c6ad7aa2276a39e3aad07e384e443c1784ac974b
Anton Korzunov <thekashey@gmail.com>
2018-08-26 21:44:51 +1000

    add timers to Phased

--- src/Phased.tsx
+++ src/Phased.tsx
@@ -11,6 +11,7 @@ interface PhaseState<T> {
 interface PhaseProps<T> {
   value: T;
   phases?: number;
+  timeouts?: number[];
   children: (state: PhaseState<T>) => React.ReactNode;
   onShift?: (state: PhaseState<T>) => any;
 }
@@ -23,24 +24,34 @@ export class Phased<T> extends React.Component<PhaseProps<T>, PhaseState<T>> {
     phasing: false,
   };
 
-  componentDidUpdate(prevProps: PhaseProps<T>) {
+  tm: number = 0;
+
+  componentDidUpdate(prevProps: PhaseProps<T>, oldState: PhaseState<T>) {
     const {value, phases = 0} = this.props;
     if (prevProps.value !== value) {
+      this.shift();
       this.setState({
         nextValue: this.props.value,
         phase: 0,
         phasing: true
-      }, this.shift);
+      });
     } else {
-      if (this.state.phase < phases) {
-        this.setState(({phase}) => ({phase: phase + 1}), this.shift);
-      } else {
-        if (this.state.value !== value) {
-          this.setState({
-            value,
-            phasing: false
-          }, this.shift);
-        }
+      if (this.state !== oldState) {
+        this.shift();
+        clearTimeout(this.tm);
+        this.tm = window.setTimeout(() => {
+          if (this.state.phase < phases) {
+            this.setState(({phase}) => ({phase: phase + 1}));
+          } else {
+            if (this.state.value !== value) {
+              this.setState(({phase}) => ({phase: phase + 1, value}))
+            } else {
+              if (this.state.phasing) {
+                this.setState({phasing: false})
+              }
+            }
+          }
+        }, (this.props.timeouts || [])[this.state.phase] || 0);
       }
     }
   }
